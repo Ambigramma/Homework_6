38ec2fdf52fa3bbad34fe3f5702138e60f4ba429
Andres Karapetyan <illum.andres@gmail.com>
2023-01-25 16:29:54 +0400

    41

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void PositiveNumbers()
+{
+    Console.Write("Введите числа через пробел: ");
+    
+    var input = Console.ReadLine();
+    var array = input.Split(' ');
+    int res = 0;
+
+    for (int i = 0; i < array.Length; i++)
+
+        {
+            if (int.Parse(array[i]) > 0) res++;
+        }
+
+    Console.WriteLine($"Количество положительных чисел: {res}");
+}
+
+PositiveNumbers();
